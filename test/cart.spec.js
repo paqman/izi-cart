36b03bdb09cee721c314b9dc6c52fef7502a5aaf
Michael P <compte@paqman.fr>
2018-11-20 16:33:56 +1300

    ✅ Testing products list

--- test/cart.spec.js
+++ test/cart.spec.js
@@ -1,3 +1,19 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
+const request = require('supertest')('http://localhost:3000')
+
+describe('Product', () => {
+  test('Get all products', () => request
+    .get('/product')
+    .expect(200)
+    .expect(({ body }) => {
+      expect(body).toHaveLength(5)
+    }))
+})
+
+describe('Cart', () => {
+  test('Get current cart', () => request
+    .get('/cart')
+    .expect(200)
+    .expect(({ body }) => {
+      expect(body).toHaveLength(0)
+    }))
 })
